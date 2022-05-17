967cbffcd56aae0edcd4d2e2d2c6d9f8767aeb4f
Blue Eu <eu.blue@pm.me>
2022-05-17 22:19:53 +0900

    [Silver IV] Title: 수 찾기, Time: 120 ms, Memory: 6776 KB -BaekjoonHub

---
+++ 백준/Silver/1920. 수 찾기/수 찾기.cc
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
+	int n,m,x;
+	scanf("%d", &n);
+	map<int, int> mymap;
+
+	for(int i=0; i<n; ++i) {
+		scanf("%d", &x);
+		mymap[x] = 1;
+	}
+
+	scanf("%d", &m);
+	for(int i=0; i<m; ++i) {
+		scanf("%d", &x);
+		if(mymap.find(x) != mymap.end()) 
+			printf("1\n");
+		else 
+			printf("0\n");
+	}
+	return 0;
+}
