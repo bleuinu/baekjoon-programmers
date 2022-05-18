14b14d5addb0ccc2a51f33b153c2c1942b7eb7f8
Blue Eu <eu.blue@pm.me>
2022-05-18 22:16:28 +0900

    [Bronze I] Title: 이항 계수 1, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11050. 이항 계수 1/이항 계수 1.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int binomialCoeff(int n, int k)
+{
+	if (k > n)
+		return 0;
+	if (k == 0 || k == n)
+		return 1;
+
+	return binomialCoeff(n - 1, k - 1)
+		+ binomialCoeff(n - 1, k);
+}
+
+int main() {
+	int x,y;
+	cin >> x >> y;
+	cout <<	binomialCoeff(x,y) << endl;
+	return 0;
+}
