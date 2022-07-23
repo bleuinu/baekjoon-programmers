440027d207a22086ab8601f293766bdd411fe323
Blue Eu <eu.blue@pm.me>
2022-07-23 09:12:22 +0900

    Update 신고 결과 받기.rb

--- 프로그래머스/lv1/92334. 신고 결과 받기/신고 결과 받기.rb
+++ 프로그래머스/lv1/92334. 신고 결과 받기/신고 결과 받기.rb
@@ -1,24 +1,19 @@
 def solution(id_list, report, k)
     gotReported = {}
     whoReported = {}
-    id_list.each { |id| gotReported[id] = 0; whoReported[id] = nil}
+    id_list.each { |id| gotReported[id] = 0; whoReported[id] = []}
 
     report.each { |str|
         src, dest = str.split(' ')
-        if whoReported[src] == nil
-            whoReported[src] = [dest]
-            gotReported[dest] += 1
-        elsif whoReported[src].include? dest
-            next
-        else
-            whoReported[src].push dest
-            gotReported[dest] += 1
-        end
+        if whoReported[src].include? dest then next end
+            
+        gotReported[dest] += 1
+        whoReported[src].push dest
     }
 
     answer = []
     id_list.each { |id|
-        if whoReported[id] == nil 
+        if whoReported[id].empty? 
             answer.push(0)
         else
             cnt = 0
